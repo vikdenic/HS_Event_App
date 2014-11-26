feb94fe592f56b1021f8f872e7e4c58f115593f4
vikdenic <vik.denic@gmail.com>
2014-11-26 13:21:51 -0600

    Initial Commit

---
+++ Event_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Event_App
+//
+//  Created by Vik Denic on 11/26/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
