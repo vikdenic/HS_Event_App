c8d92f2382d46dc84e4b41d32672a06049b4080a
vikdenic <vik.denic@gmail.com>
2014-12-11 17:08:37 -0600

    set up profile UI

---
+++ Event_App/ProfileTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileTableViewCell.swift
+//  Event_App
+//
+//  Created by Vik Denic on 12/11/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Event_App/ProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileViewController.swift
+//  Event_App
+//
+//  Created by Vik Denic on 12/11/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
